aebbeb224ccfafbcfde039069f9fb7cabe80493a
MarkovicJan <jan.markovic97@gmail.com>
2018-05-30 11:30:15 +0200

    kategorija sql

--- obrtniki.sql
+++ obrtniki.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gostitelj: 127.0.0.1
--- Čas nastanka: 30. maj 2018 ob 10.46
+-- Čas nastanka: 30. maj 2018 ob 11.29
 -- Različica strežnika: 10.1.32-MariaDB
 -- Različica PHP: 7.2.5
 
@@ -25,19 +25,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktura tabele `comments`
---
-
-CREATE TABLE `comments` (
-  `cid` int(11) NOT NULL,
-  `uid` varchar(128) COLLATE utf8_slovenian_ci NOT NULL,
-  `date` datetime NOT NULL,
-  `message` text COLLATE utf8_slovenian_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci;
-
--- --------------------------------------------------------
-
---
 -- Struktura tabele `obrtniki`
 --
 
@@ -46,31 +33,13 @@ CREATE TABLE `obrtniki` (
   `Naziv` varchar(50) CHARACTER SET utf8 NOT NULL,
   `Opis` varchar(500) CHARACTER SET utf8 NOT NULL,
   `Kontakt` varchar(50) CHARACTER SET utf8 NOT NULL,
-  `Lokacija` varchar(50) CHARACTER SET utf8 NOT NULL
+  `Lokacija` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `Kategorija` varchar(50) COLLATE utf8_slovenian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci;
-
--- --------------------------------------------------------
-
---
--- Struktura tabele `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `username` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Indeksi zavrženih tabel
 --
-
---
--- Indeksi tabele `comments`
---
-ALTER TABLE `comments`
-  ADD PRIMARY KEY (`cid`);
 
 --
 -- Indeksi tabele `obrtniki`
@@ -79,32 +48,14 @@ ALTER TABLE `obrtniki`
   ADD PRIMARY KEY (`ID_Podjetje`);
 
 --
--- Indeksi tabele `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT zavrženih tabel
 --
-
---
--- AUTO_INCREMENT tabele `comments`
---
-ALTER TABLE `comments`
-  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT tabele `obrtniki`
 --
 ALTER TABLE `obrtniki`
   MODIFY `ID_Podjetje` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT tabele `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
